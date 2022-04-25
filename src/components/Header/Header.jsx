85e14490200256c57595139d0cf5c74c70afc45d
Koki Sakai <likuor0528@gmail.com>
2022-04-25 14:50:03 -0700

    Change colors in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div>
-      <nav className='bg-white border-gray-200 px-2 sm:px-4 py-5 rounded dark:bg-gray-800'>
+      <nav className='bg-white border-gray-200 px-2 sm:px-4 py-5 rounded dark:bg-pink-500'>
         <div className='container flex flex-wrap justify-between items-center mx-auto'>
           <a href='/' className='flex items-center'>
             <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
@@ -47,8 +47,8 @@ function Header() {
             <ul className='flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium'>
               <li>
                 <a
-                  href='#'
-                  className='block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+                  href='#1'
+                  className='block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 dark:text-white'
                   aria-current='page'
                 >
                   About
@@ -56,8 +56,8 @@ function Header() {
               </li>
               <li>
                 <a
-                  href='#'
-                  className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                  href='#2'
+                  className='block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
                 >
                   ブランド一覧
                 </a>
