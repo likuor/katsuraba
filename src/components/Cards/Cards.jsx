c424aa2d89721982bad59c5903ac79481b1bf357
Koki Sakai <likuor0528@gmail.com>
2022-04-29 11:26:14 -0700

    Add restaurant info

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -15,12 +15,11 @@ function Cards(props) {
               </a>
               <div className='p-5'>
                 <a href='/'>
-                  <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                    Noteworthy technology acquisitions 2021
+                  <h5 className='mb-2 text-base font-bold tracking-tight text-gray-900 dark:text-white'>
+                    {restaurant.name}
                   </h5>
-                  <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
-                    Here are the biggest enterprise technology acquisitions of
-                    2021 so far, in reverse chronological order.
+                  <p className='mb-3 font-normal text-xs text-gray-700 dark:text-gray-400'>
+                    {restaurant.description}
                   </p>
                 </a>
               </div>
