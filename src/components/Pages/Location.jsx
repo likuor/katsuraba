30799cadf2214404157e3473567a7521c43783e9
Koki Sakai <likuor0528@gmail.com>
2022-04-30 16:21:53 -0700

    Add location detail page

--- src/components/Pages/Location.jsx
+++ src/components/Pages/Location.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import Title from '../Title/Title';
+import React, { useEffect, useState } from 'react';
+import RestaurantTitle from '../Title/LocationTitle';
+import { useParams } from 'react-router-dom';
 
-function Location() {
+function Location(props) {
+  const { id } = useParams();
+  const [restaurant, setRestaurant] = useState('');
+
+  useEffect(() => {
+    const res = props.RestaurantsDataList.filter((restaurant) => {
+      return restaurant.id === Number(id);
+    });
+    setRestaurant(res[0]);
+  }, [props, id]);
+
   return (
     <div>
-      <Title
-        mainTitle='店名'
-        mainDescription='住所'
-        subDescription='お店の説明'
-      />
+      <div className='container mx-auto py-20 px-6'>
+        <img src={restaurant.img} alt='' />
+        <RestaurantTitle
+          mainTitle={restaurant.name}
+          subTitle={restaurant.address}
+          mainDescription={restaurant.branch}
+          subDescription={restaurant.description}
+        />
+      </div>
+      <div>
+        {restaurant?.locations?.map((restaurant, index) => {
+          return (
+            <div>
+              {restaurant.address}
+              {restaurant.branch}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
