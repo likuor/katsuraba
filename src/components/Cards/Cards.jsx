66f5663ee4c564820d3a3bf1dfdf55cf285e1a1a
Koki Sakai <likuor0528@gmail.com>
2022-05-09 11:23:39 -0700

    Add responsive css on card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,33 +4,34 @@ import Underline from '../Underline/Underline';
 
 function Cards(props) {
   return (
-    <div className='container mx-auto py-10 px-6'>
+    <div className='container mx-auto py-10'>
       <div className='text-center text-gray-800 py-10 px-6'>
         <div className='container mx-auto'>
-          <section className='text-gray-800 text-center'>
+          <section>
             <h1 className='text-3xl font-bold mb-4'>ブランド一覧</h1>
             <Underline />
           </section>
         </div>
       </div>
-      <div className='grid gap-4 grid-cols-3'>
+      <div className='grid gap-4 md:grid-cols-3 grid-cols-1 px-6'>
         {props.RestaurantsDataList.map((restaurant, index) => {
           return (
-            <div
-              className='bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'
-              key={index}
-            >
+            <div className='rounded-lg shadow-md bg-gray-800' key={index}>
               <Link to={`/Location/${index}`}>
-                <img className='rounded-t-lg' src={restaurant.img} alt='' />
+                <img
+                  className='rounded-t-lg w-full'
+                  src={restaurant.img}
+                  alt=''
+                />
               </Link>
               <div className='p-5'>
                 <Link to={`/Location/${index}`}>
-                  <h5 className='mb-2 text-base font-bold tracking-tight text-gray-900 dark:text-white'>
+                  <h5 className='mb-2 text-base font-bold tracking-tight text-white'>
                     {restaurant.name}
                   </h5>
                 </Link>
                 <Link to={`/Location/${index}`}>
-                  <p className='mb-1 font-normal text-xs text-gray-700 dark:text-gray-400'>
+                  <p className='mb-1 font-normal text-xs text-gray-400'>
                     {restaurant.description}
                   </p>
                 </Link>
