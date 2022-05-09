5435e8617e2ee9969bbb8bd66aaf435a8c33c796
Koki Sakai <likuor0528@gmail.com>
2022-05-08 22:38:16 -0700

    Refactor css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 function Footer() {
   return (
     <div>
-      <footer className='p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800'>
+      <footer className='p-4 shadow md:px-7 md:py-8 bg-gray-800'>
         <div className='sm:flex sm:items-center sm:justify-between'>
           <a href='/' className='flex items-center mb-4 sm:mb-0'>
-            <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+            <span className='self-center text-2xl font-semibold whitespace-nowrap text-white'>
               かつらばキッチン
             </span>
           </a>
-          <ul className='flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400'>
+          <ul className='flex flex-wrap items-center mb-6 text-sm sm:mb-0 text-white'>
             <li>
               <a href='/' className='mr-4 hover:underline md:mr-6 '>
                 About
@@ -28,8 +28,8 @@ function Footer() {
             </li>
           </ul>
         </div>
-        <hr className='my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8' />
-        <span className='block text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+        <hr className='my-6 sm:mx-auto border-gray-700 lg:my-8' />
+        <span className='block text-sm text-white sm:text-center '>
           © 2022{' '}
           <a href='/' className='hover:underline'>
             かつらばキッチン
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,17 +4,17 @@ import React from 'react';
 function Header() {
   return (
     <div>
-      <nav className='bg-white border-gray-200 px-2 sm:px-4 py-5 rounded dark:bg-pink-500'>
-        <div className='container flex flex-wrap justify-between items-center mx-auto'>
+      <nav className='border-gray-200 px-2 sm:px-4 py-5 bg-pink-500'>
+        <div className='container flex flex-wrap justify-between items-center place-content-between'>
           <Link className='flex items-center' to='/'>
-            <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
+            <span className='self-center text-xl font-semibold whitespace-nowrap text-white ml-3'>
               かつらばキッチン
             </span>
           </Link>
           <button
             data-collapse-toggle='mobile-menu'
             type='button'
-            className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+            className='inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600'
             aria-controls='mobile-menu'
             aria-expanded='false'
           >
